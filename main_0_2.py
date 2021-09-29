ee923be27ac50e842a0fb945da81bdbde51a13c3
bilas <dmitry.bilas@gmail.com>
2021-09-29 08:26:33 +0300

    commit

--- main_0_2.py
+++ main_0_2.py
@@ -7,9 +7,6 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 import sys
 
 
-
-
-
 class MyMainWindow(QMainWindow,Ui_MainWindow):
     def __init__(self,parent=None):
         super(MyMainWindow, self).__init__(parent)
